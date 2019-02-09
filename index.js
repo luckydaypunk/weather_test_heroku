8952e64e3108449e12523267bb3faee43723b4ee
luckydaypunk <christopher.greene24@mail.dcu.ie>
2019-02-09 15:40:49 +0900

    adding Darksky API

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const request = require("request")
-const moment = require('moment')
-const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
 express()
+  .use(allowCrossDomain)
   .get('/', async (req, res) => {
     let ip = req.headers['x-forwarded-for']
     if (ip){
